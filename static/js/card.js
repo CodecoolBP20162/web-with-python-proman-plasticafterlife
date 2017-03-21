996f7b3db35f73cc4d07a31817784c37f900a8e3
keligeri <kel.gergo@gmail.com>
2017-03-21 14:05:13 +0100

    Card works fine, but try another implementation

--- static/js/card.js
+++ static/js/card.js
@@ -1,17 +1,43 @@
 
 // Card constructor
 function Card(cardTitle) {
-    this.cardTitle = cardTitle;
-    this.content = "";
     this.cardDate = new Date();
     this.cardId = this.cardDate.valueOf();
-    this.status = "New";
+    this.content = "";
 }
 
+// status constructor
+function Status(statusTitle) {
+    this.statusTitle = statusTitle;
+    this.cardList = [];
+};
 
-//object example
-var card = new Card('First card')
-console.log(card.status);
-console.log(card.cardDate);
-console.log(card.cardId);
+//status and card instance
+var newStatus = new Status('new');
+var card = new Card('First card');
+
+$(document).ready(function() {
+
+    var saveCard = function(statusObj, cardObject) {        // Save card in status function
+        statusObj.cardList.push(cardObject);
+    };
+
+    var addContentToCard = function(cardObj, content) {     // add content to the cardObj - ect
+        cardObj.content += content.value;
+    };
+
+    var $saveButton = $("#save_button");    // select the save_button then call the saveCard function
+    $saveButton.click(function () {
+        var inputTask = document.getElementById('new_task');      // select the input field element
+        addContentToCard(card, inputTask);                  // save to content
+
+        saveCard(newStatus, card);
+
+        console.log(newStatus.cardList);
+        console.log("Save something");
+    });    // end of save function
+
+});
+
+
 
