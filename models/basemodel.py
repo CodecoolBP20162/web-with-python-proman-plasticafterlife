acb3b6a8ea49d2253cf5b14e79e1b2879db7124b
Fanni Skoda <skoda.fanni@gmail.com>
2017-04-06 11:00:02 +0200

    deploy app

--- models/basemodel.py
+++ models/basemodel.py
@@ -7,7 +7,9 @@ database_name = fileread.database_name()
 database_user = fileread.database_user()
 
 
-db = PostgresqlDatabase(database_name, user=database_user)
+db = PostgresqlDatabase(database_name, user=database_user,
+                        password="a21add82ac8665df2664ca79e334c99fd8728cd04840bc6ab4a9b43be053c777",
+                        host="ec2-79-125-118-221.eu-west-1.compute.amazonaws.com")
 
 
 class BaseModel(Model):
